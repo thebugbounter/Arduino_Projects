7752e7c27266ff637d3cb33783c61b53740a41d2
JAS PREET <89130631+thebugbounter@users.noreply.github.com>
2025-01-12 18:43:42 +0530

    v1

---
+++ fake_update.ino
@@ -0,0 +1,22 @@
+#include "DigiKeyboard.h"
+void setup() {
+  //empty
+}
+void loop() {
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);  //Abre o executar
+  DigiKeyboard.delay(600);
+  DigiKeyboard.print("https://www.youtube.com/watch?v=k0J6MeEfFAc");  //Abre o video
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(5000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);  //Abre o executar
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.print("http://fakeupdate.net/win10ue"); //Abre o site
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_F11);
+  for (;;) {
+    /*empty*/
+  }
+}
